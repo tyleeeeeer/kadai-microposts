17fae7cab90dc7bc88d3d9661157788b480133b5
tyler <tyler.github2018@gmail.com>
2018-06-14 07:37:10 +0000

    timeline

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -14,17 +14,14 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
             $data = [
                 'user' => $user,
                 'microposts' => $microposts,
             ];
-            $data += $this->counts($user);
-            return view('users.show', $data);
-        }else {
-            return view('welcome');
         }
+        return view('welcome', $data);
     }
     
     public function store(Request $request)
@@ -50,4 +47,6 @@ class MicropostsController extends Controller
 
         return redirect()->back();
     }
+    
+    
 }
--- app/Micropost.php
+++ app/Micropost.php
@@ -12,4 +12,7 @@ class Micropost extends Model
     {
         return $this->belongsTo(User::class);
     }
+
 }
+
+
